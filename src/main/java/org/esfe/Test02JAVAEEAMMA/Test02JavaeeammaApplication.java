f5191f01cd77c8df18db3cca09ae04f8f729046e
Ángel Miguel Mena Argumedo <angelmeba4@gmail.com>
2024-08-07 19:02:02 -0600

    Creacion proyecto

---
+++ src/main/java/org/esfe/Test02JAVAEEAMMA/Test02JavaeeammaApplication.java
@@ -0,0 +1,13 @@
+package org.esfe.Test02JAVAEEAMMA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test02JavaeeammaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test02JavaeeammaApplication.class, args);
+	}
+
+}
